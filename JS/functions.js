1dec2e9d6d87f7b42ba7662fcb3a1b63d2d88af3
Jonathan Garcia <jonathangarciaconesa@gmail.com>
2023-11-28 13:07:38 +0100

    Ejemplo con la nav en la barra lateral

--- JS/functions.js
+++ JS/functions.js
@@ -5,14 +5,11 @@
  * 
  * 
  */
+/*******************function sidenav responsive    */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-/********************** function tovnav responsive  */
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
